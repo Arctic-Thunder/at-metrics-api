92d8b8a2b7a2c01f6f41f624c9a05eba87f7dc50
Ryan Thally <thally@etsu.edu>
2019-10-04 18:29:40 -0400

    Exposed network ports

--- cscimetrics/metrics-root/metrics-api/settings.py
+++ cscimetrics/metrics-root/metrics-api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'metrics'
+    'metrics',
 ]
 
 REST_FRAMEWORK = {
--- cscimetrics/metrics-root/metrics/views.py
+++ cscimetrics/metrics-root/metrics/views.py
@@ -4,10 +4,6 @@ from rest_framework.response import Response
 from .models import Metric, MetricType
 from .serializers import MetricSerializer, MetricTypeSerializer, UserSerializer
 
-
-# Create your views here.
-
-
 class MetricList(generics.ListCreateAPIView):
     queryset = Metric.objects.all()
     serializer_class = MetricSerializer
@@ -27,15 +23,3 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
-
-# class LoginView(views.APIView):
-#     permission_classes = ()
-#
-#     def post(self, request):
-#         uname = request.data.get('username')
-#         passwd = request.data.get('password')
-#         user = authenticate(username=uname, password=passwd)
-#         if user:
-#             return Response({"token": user.auth_token_key})
-#         else:
-#             return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
