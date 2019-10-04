b37ca77c372c5551924d077149ad2ad69ed3c91e
Ryan Thally <thally@etsu.edu>
2019-10-04 10:44:48 -0400

    Clean up

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rthally94/Library/Mobile Documents/com~apple~CloudDocs/Computing/SE2/cscimetrics/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
