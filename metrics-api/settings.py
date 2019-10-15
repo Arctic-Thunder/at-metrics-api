7ad618642b85710c970dfcaa09af487b0a0b417b
Ryan Thally <rthally94@gmail.com>
2019-10-14 22:41:14 -0400

    Disable debug

--- metrics-api/settings.py
+++ metrics-api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8q3suijp&b1(hvrttiuai0l#9kvkr#wdvx&4)2aqsr564c*e25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
