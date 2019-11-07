22c5962d9b6bc86ff1b308b4f02ebf1ecd870183
rthally94 <thally@etsu.edu>
2019-11-07 14:58:34 -0500

    Update settings.py

--- metrics-api/settings.py
+++ metrics-api/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'metrics-api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
