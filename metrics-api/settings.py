1b7089853f8217c7db474d8ef451121092bbcbd5
rthally94 <thally@etsu.edu>
2019-11-07 14:49:00 -0500

    Update settings.py

--- metrics-api/settings.py
+++ metrics-api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8q3suijp&b1(hvrttiuai0l#9kvkr#wdvx&4)2aqsr564c*e25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
